36dbabcba7c896f21fe3c66637edaceb874be2b2
Douglas Strey <douglas.morini@hotmail.com>
2021-07-22 14:19:36 -0300

    Iniciado exercício 03

---
+++ aula12/02-exercicio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="_css/estilo.css" />
+    <meta charset="UTF-8" />
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+
+<body>
+    <div>
+        <?php
+            // contagem regressiva
+            $c = 10;
+
+            do {
+                echo " $c";
+                $c--;
+            } while ($c >= 1);
+        ?>
+    </div>
+</body>
+
+</html>
